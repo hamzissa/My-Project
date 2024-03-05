ea1f48fa36b41d3d3aab3c925a54809f786dd3b6
hamzissa <125409932+hamzissa@users.noreply.github.com>
2024-03-05 22:06:05 +0000

    made a basic main menu start and quit button work

--- my project/Scenes/menu/menu.gd
+++ my project/Scenes/menu/menu.gd
@@ -1,5 +1,20 @@
 extends Control
 
+class_name MainMenu
 
-func _on_start_button_pressed():
-	get_tree().change_scene_to_file(("res://Scenes/map/map.tscn"))
+@onready var play_button = $MarginContainer/HBoxContainer/VBoxContainer/Play as Button
+@onready var settings_button =$MarginContainer/HBoxContainer/VBoxContainer/Settings as Button
+@onready var quit_button =$MarginContainer/HBoxContainer/VBoxContainer/Quit as Button
+@onready var map = preload("res://Scenes/map/map.tscn") as PackedScene
+#func _on_start_button_pressed():
+	#get_tree().change_scene_to_file(("res://Scenes/map/map.tscn"))
+
+func _ready():
+	play_button.button_down.connect(on_play)
+	quit_button.button_down.connect(on_exit)
+
+func on_play() -> void:
+	#get_tree().change_scene_to_file(("res://Scenes/map/map.tscn"))
+	get_tree().change_scene_to_packed(map)
+func on_exit() -> void:
+	get_tree().quit()
