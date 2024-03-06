cd21be25eb61255ebd9dcf6da36bf6b632d2a4ab
hamzissa <125409932+hamzissa@users.noreply.github.com>
2024-03-06 14:43:54 +0000

    added settings button
    shows controls

--- my project/Scenes/menu/menu.gd
+++ my project/Scenes/menu/menu.gd
@@ -6,15 +6,34 @@ class_name MainMenu
 @onready var settings_button =$MarginContainer/HBoxContainer/VBoxContainer/Settings as Button
 @onready var quit_button =$MarginContainer/HBoxContainer/VBoxContainer/Quit as Button
 @onready var map = preload("res://Scenes/map/map.tscn") as PackedScene
+@onready var settings_menu = $settings_menu as settingsMenu
+@onready var margin_container =$MarginContainer as MarginContainer
 #func _on_start_button_pressed():
 	#get_tree().change_scene_to_file(("res://Scenes/map/map.tscn"))
 
 func _ready():
-	play_button.button_down.connect(on_play)
-	quit_button.button_down.connect(on_exit)
+	handle_connection_of_signals()
 
 func on_play() -> void:
 	#get_tree().change_scene_to_file(("res://Scenes/map/map.tscn"))
 	get_tree().change_scene_to_packed(map)
-func on_exit() -> void:
+
+func on_settings() -> void:
+	margin_container.visible = false
+	settings_menu.set_process(true)
+	settings_menu.visible = true
+
+func on_quit() -> void:
 	get_tree().quit()
+	
+
+func on_back_settings_menu() -> void:
+	margin_container.visible = true
+	settings_menu.visible = false
+	
+
+func handle_connection_of_signals() -> void:
+	play_button.button_down.connect(on_play)
+	settings_button.button_down.connect(on_settings)
+	quit_button.button_down.connect(on_quit)
+	settings_menu.exit_settings_menu.connect(on_back_settings_menu)
---
+++ my project/Scenes/settings_menu/settings_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+class_name settingsMenu
+
+@onready var back_button = $MarginContainer/VBoxContainer/back as Button
+
+signal exit_settings_menu
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	back_button.button_down.connect(on_back_button)
+	set_process(false) 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func on_back_button() -> void:
+	exit_settings_menu.emit()
+	set_process(false) 
