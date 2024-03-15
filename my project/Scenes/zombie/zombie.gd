6fa11685f2a54e7a6e967b2ccd2455b29150b9dc
Shajid Issa <saj.issa@gmail.com>
2024-03-15 00:12:39 +0000

    score

--- my project/Scenes/zombie/zombie.gd
+++ my project/Scenes/zombie/zombie.gd
@@ -4,8 +4,8 @@ class_name Zombie
 
 @onready var sprite = $Sprite2D
 @onready var ai = $AI
-
-
+@onready var health_bullet_ui: CanvasLayer = get_node("../health_bullet_UI") # Adjust the path according to your scene structure
+@onready var score_label: Label = health_bullet_ui.get_node("VBoxContainer").get_node("bottom").get_node("score")
 
 var health = 100
 
@@ -18,3 +18,4 @@ func handle_hit():
 	print("enemy hit!", health)
 	if health<= 0:
 		queue_free()
+		score_label.text = str(int(score_label.text) + 1)
